ef675205a8014dcd888b3d64907c8f5024759352
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2013-01-29 22:08:47 -0430

    Initial commit

---
+++ blitzkrieg-desktop/src/ve/com/alericoveri/blitzkrieg/Main.java
@@ -0,0 +1,16 @@
+package ve.com.alericoveri.blitzkrieg;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "blitzkrieg";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new Blitzkrieg(), cfg);
+	}
+}
