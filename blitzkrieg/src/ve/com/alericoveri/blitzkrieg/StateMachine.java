88149a71298d3e68d1658e67975cb5bcd9d6a85b
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2013-02-04 14:33:19 -0430

    Code formatting

--- blitzkrieg/src/ve/com/alericoveri/blitzkrieg/StateMachine.java
+++ blitzkrieg/src/ve/com/alericoveri/blitzkrieg/StateMachine.java
@@ -32,77 +32,71 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /**
  * 
- * @author alejandro
- *
  */
-public class StateMachine 
-{
+public class StateMachine {
 	/**
 	 * 
-	 * @author alejandro
-	 *
 	 */
-	abstract public static class State<T> 
-	{
+	abstract public static class State<T> {
 		/** */
 		private StateMachine mParent;
-		
+
 		/** */
 		protected T mObject;
-		
+
 		/** */
 		protected String mKey;
-		
+
 		/** */
 		abstract public void onEnter();
-		
+
 		/** */
 		abstract public void onExec(SpriteBatch batch, float parentAlpha);
-		
+
 		/** */
 		abstract public void onExit();
-		
+
 		/** */
-		public void pop () {
+		public void pop() {
 			mParent.pop();
 		}
-		
+
 		/** */
-		public void push (String stateName) {
+		public void push(String stateName) {
 			mParent.push(stateName);
 		}
-		
+
 		/** */
-		public State (String key, StateMachine parent, T object) {
+		public State(String key, StateMachine parent, T object) {
 			mParent = parent;
 			mObject = object;
 			mKey = key;
 		}
-		
+
 		public String getKey() {
 			return mKey;
 		}
 	}
-	
-	/** */ 
-	private Stack<State> mStates;
-	
+
 	/** */
-	private TreeMap<String,State> mRegStates;
-	
+	private Stack<State> mStates;
+
+	/** */
+	private TreeMap<String, State> mRegStates;
+
 	/** */
 	private String mImpName;
-	
+
 	/** */
-	public StateMachine (String impName) {
-		mImpName 	= impName;
-		mStates 	= new Stack<State>();
-		mRegStates 	= new TreeMap<String, State>();
-		Gdx.app.log("StateMachine","new implementation '"+mImpName+"'");
+	public StateMachine(String impName) {
+		mImpName = impName;
+		mStates = new Stack<State>();
+		mRegStates = new TreeMap<String, State>();
+		Gdx.app.log("StateMachine", "new implementation '" + mImpName + "'");
 	}
-	
+
 	/** */
-	public void pop () {
+	public void pop() {
 		if (!mStates.empty()) {
 			mStates.firstElement().onExit();
 			mStates.pop();
@@ -110,9 +104,9 @@ public class StateMachine
 				mStates.firstElement().onEnter();
 		}
 	}
-	
+
 	/** */
-	public void push (String stateName) {
+	public void push(String stateName) {
 		if (!mRegStates.isEmpty()) {
 			if (mRegStates.containsKey(stateName)) {
 				if (!mStates.empty())
@@ -122,30 +116,30 @@ public class StateMachine
 			}
 		}
 	}
-	
+
 	/** */
-	public void swap (String stateName) {
+	public void swap(String stateName) {
 		if (mStates.firstElement().getKey() != stateName) {
 			pop();
 			push(stateName);
 		}
 	}
-	
+
 	/** */
-	public State<?> getCurrentState () {
+	public State<?> getCurrentState() {
 		if (!mStates.empty())
 			return mStates.firstElement();
 		return null;
 	}
-	
+
 	/** */
-	public void register (State<?> state) {
-			if (!mRegStates.containsKey(state.getKey()))
-				mRegStates.put(state.getKey(), state);
+	public void register(State<?> state) {
+		if (!mRegStates.containsKey(state.getKey()))
+			mRegStates.put(state.getKey(), state);
 	}
-	
+
 	/** */
-	public void run (SpriteBatch batch, float parentAlpha) {
+	public void run(SpriteBatch batch, float parentAlpha) {
 		if (!mStates.empty())
 			mStates.firstElement().onExec(batch, parentAlpha);
 	}
